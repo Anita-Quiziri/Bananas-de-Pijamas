d9b1214e2c0635187d27d73526667ba6bff7aa6e
Anita-Quiziri <isabelaquiziri94@gmail.com>
2025-06-18 08:49:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function enviar() {
+    alert("Acesso realizado com sucesso!")
+}
+
+// variavel pra pegar o nome do visitante 
+var usuario = prompt("Digite o seu nome: ")
+
+// pegar o valor da variavel e imprimir na tela
+document.getElementById("Visitante").innerHTML = usuario
